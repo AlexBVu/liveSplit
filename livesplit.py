05b32b38f14c1893ac38fea9fe1d97146c892a07
Alex Vu <alexvuster24@gmail.com>
2026-08-26 23:40:46 -0700

    parsed input

--- livesplit.py
+++ livesplit.py
@@ -5,19 +5,32 @@ class Livesplit(rumps.App):
     def __init__(self, selfquit_button='Quit'):
         super(Livesplit, self).__init__("LiveSplit")
         self.menu = ["Start run"]
+
+        self.segments = []
+
         rumps.debug_mode("On")
 
     @rumps.clicked("Start run")
     def start_run(self, sender):
         # rumps.notification("starting run", "Subtitle", "go!")
-        window = rumps.Window("window", "title", default_text=" ", ok=None, cancel=None, 
+        window = rumps.Window("window", "title", default_text="", ok=None, cancel=None, 
                      dimensions=(320, 160))
 
         window.add_button("start")
-        response = window.run()
-        print(response.text)
 
-        # TODO: parse response, add segments
+        response = window.run()
+        r = response.text.split()
+
+        self.add_segments(r)
+
+
+    def add_segments(self, response: list):
+        for i in range(0, len(response), 2):
+            seg = response[i]
+            time = response[i + 1]
+            self.segments.append((seg,time))
+
+    
 
 
 def main():
