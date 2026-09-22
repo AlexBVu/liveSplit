7ace10e8def2a6b1a1becb6cff88d8349d553f7d
Alex Vu <alexvuster24@gmail.com>
2026-09-22 13:35:06 +0700

    things are broken --> not displaying name now

--- livesplit.py
+++ livesplit.py
@@ -1,37 +1,52 @@
 import rumps
-import time
+import datetime
+
 
 class Livesplit(rumps.App):
     def __init__(self, selfquit_button='Quit'):
         super(Livesplit, self).__init__("LiveSplit")
-        self.menu = ["Start run"]
+        self.menu = ["Add Segments", "Start Run"]
 
-        self.segments = []
+        self.task = None
+        self.time = None
+
+        # self.segments = [] ? do i need this ? 
 
         rumps.debug_mode("On")
 
-    @rumps.clicked("Start run")
-    def start_run(self, sender):
+    @rumps.clicked("Add Segments")
+    def add_segments(self, sender):
         # rumps.notification("starting run", "Subtitle", "go!")
-        window = rumps.Window("window", "title", default_text="", ok=None, cancel=None, 
+        window = rumps.Window("Format: [event-minutes;]", "Enter Your Segments:", default_text="", ok=None, cancel=None, 
                      dimensions=(320, 160))
-
-        window.add_button("start")
 
         response = window.run()
         r = response.text.split()
 
-        self.add_segments(r)
+        self.add_segments_helper(r)
 
+    @rumps.clicked("Start Run")
+    def start_run(self):
+        self.update()
+        self.title = "why am i here"
 
-    def add_segments(self, response: list):
-        for i in range(0, len(response), 2):
-            seg = response[i]
-            time = response[i + 1]
-            self.segments.append((seg,time))
+    @rumps.timer(3)
+    def update(self):
+        if self.task and self.time:
+            self.title = self.task + " " + str(self.time)
+            self.time -= 1
+        else:
+            self.title = "error: either are null"
 
-    
-
+    def add_segments_helper(self, response: list):
+        self.task = response[0]
+        self.time = response[1]
+        # for i in range(0, len(response), 2):
+        #     task = response[i]
+        #     time = response[i + 1]
+        #     self.segments.append((task,time))
+        #     self.task = task
+        #     self.time = int(time)
 
 def main():
     Livesplit().run()
